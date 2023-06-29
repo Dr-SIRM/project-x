6116a28d07b926b5063842631f182a73fa6cb983
gerymueller <126329287+gerymueller@users.noreply.github.com>
2023-06-29 08:42:38 +0000

    Alles Umgebaut in eine Klasse

--- or_algorithm.py
+++ or_algorithm.py
@@ -45,9 +45,355 @@ class ORAlgorithm:
         self.time_req = dp.time_req
         self.user_employment = dp.user_employment
 
+        # Attribute der Methode "create_variables"
+        self.mitarbeiter = None
+        self.verfügbarkeit = None
+        self.kosten = None
+        self.max_zeit = None
+        self.min_zeit = None
+        self.working_h = None
+        self.calc_time = None
+        self.employment_lvl_list = None
+        self.employment = None
+        self.verteilbare_stunden = None
+        self.stunden_pro_tag = None
+        self.gesamtstunden_verfügbarkeit = None
+        self.min_anwesend = None
+
+        # Attribute der Methode "solver_selection"
+        self.solver = None
+
+        # Attribute der Methode "decision_variables"
+        self.x = None
+        self.y = None
+        self.s = None
+        self.a = None
+
+        # Attribute der Methode "objective_function"
+        self.objective = None
+
+        # Attribute der Methode "objective_function"
+        self.status = None
+
 
     def run(self):
+        self.create_variables()
         self.algorithm()
+        # Hier alle Methoden runnen lassen
+
+
+    def create_variables(self):
+        """
+        Allgemeine Variabeln
+        """
+
+        # user_ids Liste, wird als Key in der Ausgabe verwendet
+        self.mitarbeiter = [user_id for user_id in self.binary_availability]
+
+        # Aus dem binary_availability dict. die Verfügbarkeits-Informationen ziehen
+        self.verfügbarkeit = {}
+        for i, (user_id, availabilities) in enumerate(self.binary_availability.items()):
+            self.verfügbarkeit[self.mitarbeiter[i]] = []
+            for day_availability in availabilities:
+                date, binary_list = day_availability
+                self.verfügbarkeit[self.mitarbeiter[i]].append(binary_list)
+
+        # Kosten für jeden MA noch gleich, ebenfalls die max Zeit bei allen gleich
+        self.kosten = {ma: 20 for ma in self.mitarbeiter}  # Kosten pro Stunde
+
+        self.max_zeit = {ma: 8 for ma in self.mitarbeiter}  # Maximale Arbeitszeit pro Tag
+        self.min_zeit = {ma: 2 for ma in self.mitarbeiter}  # Minimale Arbeitszeit pro Tag
+        # max Stunden pro MA pro Woche - Kann evtl. noch aus der Datenbank gezogen werden in Zukunft?
+        self.working_h = 35
+
+        # Berechnung der calc_time (Anzahl Tage an denen die MA eingeteilt werden)
+        # Es werden nur die Tage des ersten MA berechnet, da jeder MA die gleiche Wochenlänge hat
+        self.calc_time = len(next(iter(self.binary_availability.values())))
+
+        # Shifts aus dem Attribut der Variable shifts zuweisen
+        self.shifts = self.company_shifts
+
+        # Empolyment_level aus dem employment_lvl dict in einer Liste speichern (nur MA die berücksichtigt werden)
+        self.employment_lvl = [] 
+
+        # Iterieren Sie über die Schlüssel in binary_availability
+        for user_id in self.binary_availability.keys():
+            # Prüfen Sie, ob die user_id in employment_lvl vorhanden ist
+            if user_id in self.employment_lvl:
+                # Fügen Sie den entsprechenden employment_lvl Wert zur Liste hinzu
+                self.employment_lvl.append(self.employment_lvl[user_id])
+        print("Liste Empolyment_lvl: ", self.employment_lvl)
+
+        self.employment_lvl = [1, 0.8, 0.8, 0.6, 0.6] # Damit die Liste noch selbst manipuliert werden kann.
+
+        """
+        # Creating a Employment List based on the user of binary_availability
+        employment = []
+
+        # Iteration of the key within binary_availability
+        for user_id in self.binary_availability.keys():
+            if user.id in self.user_employment:
+                employment.append(self.user_employment[user_id])
+        print("List Employment: ", employment)
+        """
+        self.employment = ["Perm", "Temp", "Temp", "Temp", "Temp"]
+
+        # verteilbare Stunden (Wieviele Mannstunden benötigt die Firma im definierten Zeitraum)
+        self.verteilbare_stunden = 0
+        for date in self.time_req:
+            for hour in self.time_req[date]:
+                self.verteilbare_stunden += self.time_req[date][hour]
+        print("Verteilbare Stunden: ", self.verteilbare_stunden)
+
+        # gesamtstunden Verfügbarkeit pro MA pro Woche
+        self.stunden_pro_tag = 1 # flexibler wenn einmal 1/4h eingebaut werden
+
+        self.gesamtstunden_verfügbarkeit = []
+        for key in self.binary_availability:
+            gesamt_stunden = sum(sum(day_data[1]) * stunden_pro_tag for day_data in self.binary_availability[key])
+            self.gesamtstunden_verfügbarkeit.append(gesamt_stunden)
+
+        print("Gesamtstunden Verfügbarkeit: ", self.gesamtstunden_verfügbarkeit)
+
+        # Eine Liste mit den min. anwesendheiten der MA wird erstellt
+        for _, values in sorted(self.time_req.items()):
+            self.min_anwesend.append(list(values.values()))
+
+       
+    def solver_selection(self):
+        """
+        Anwahl des geeigneten Solvers
+        # GLOP = Simplex Verfahren
+        # CBC =  branch-and-bound- und branch-and-cut-Verfahren
+        # SCIP = Framework für die Lösung gemischt-ganzzahliger Programmierungsproblem
+        # GLPK = Vielzahl von Algorithmen, einschließlich des Simplex-Verfahrens und des branch-and-bound-Verfahrens
+        """
+        self.solver = pywraplp.Solver.CreateSolver('SCIP')
+
+
+    def decision_variables(self):
+        """ 
+        Entscheidungsvariabeln 
+        # solver.NumVar() <-- kontinuierliche Variabeln
+        # solver.BoolVar() <-- boolesche Variabeln
+        # solver.IntVar() <-- Int Variabeln
+        """
+
+        # Arbeitsvariable
+        self.x = {}
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):  # Für jeden Tag der Woche
+                for k in range(len(self.verfügbarkeit[i][j])):  # Für jede Stunde des Tages, an dem die Firma geöffnet ist
+                    self.x[i, j, k] = self.solver.IntVar(0, 1, f'x[{i}, {j}, {k}]') # Variabeln können nur die Werte 0 oder 1 annehmen
+
+        # Arbeitsblockvariable
+        self.y = {}
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):  # Für jeden Tag der Woche
+                for k in range(len(self.verfügbarkeit[i][j])):  # Für jede Stunde des Tages, an dem die Firma geöffnet ist
+                    self.y[i, j, k] = self.solver.IntVar(0, 1, f'y[{i}, {j}, {k}]') # Variabeln können nur die Werte 0 oder 1 annehmen
+
+        # Schichtvariable - wird noch nicht genutzt!
+        self.s = {}
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):  # Für jeden Tag der Woche
+                for k in range(len(self.verfügbarkeit[i][j])):  # Für jede Stunde des Tages, an dem die Firma geöffnet ist
+                    self.s[i, j, k] = self.solver.IntVar(0, 1, f'y[{i}, {j}, {k}]') # Variabeln können nur die Werte 0 oder 1 annehmen
+
+        # Arbeitstagvariable
+        self.a = {}
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):  # Für jeden Tag der Woche
+                self.a[i, j] = self.solver.BoolVar(f'a[{i}, {j}]') # Variablen können nur die Werte 0 oder 1 annehmen
+
+
+    def objective_function(self):
+        """
+        Zielfunktion
+        """
+        self.objective = solver.Objective()
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):
+                for k in range(len(self.verfügbarkeit[i][j])):
+                    # Die Kosten werden multipliziert
+                    self.objective.SetCoefficient(x[i, j, k], self.kosten[i])
+        # Es wird veruscht, eine Kombination von Werten für die x[i, j, k] zu finden, die die Summe kosten[i]*x[i, j, k] minimiert            
+        self.objective.SetMinimization()
+
+
+    def pre_check(self):
+        """ 
+        Vorüberprüfungen
+        """
+        # Hat Phu noch hinzugefügt, haben die festen MA genug Stunden eingeplant?
+        for i in range(len(self.mitarbeiter)):
+            if self.employment[i] == "Perm": 
+                sum_availability_perm = 0
+                for j in range(self.calc_time):
+                    for k in range(len(self.verfügbarkeit[i][j])):
+                        sum_availability_perm += self.verfügbarkeit[i][j][k]
+                        print(sum_availability_perm)           
+                if sum_availability_perm <= self.working_h:
+                    print(self.mitarbeiter[i], " has not planned enough hours.")
+                else:
+                    pass
+
+    def constraints(self):
+        """
+        Beschränkungen / Nebenbedingungen
+        # (Die solver.Add() Funktion nimmt eine Bedingung als Argument und fügt sie dem Optimierungsproblem hinzu.)
+        """
+
+        # NB 1 - MA nur einteilen, wenn er verfügbar ist.
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):
+                for k in range(len(self.verfügbarkeit[i][j])):
+                    self.solver.Add(x[i, j, k] <= self.verfügbarkeit[i][j][k])
+
+
+        # NB 2 - Mindestanzahl MA zu jeder Stunde an jedem Tag anwesend 
+        for j in range(self.calc_time):
+            for k in range(len(self.verfügbarkeit[self.mitarbeiter[0]][j])):  # Wir nehmen an, dass alle Mitarbeiter die gleichen Öffnungszeiten haben
+                self.solver.Add(self.solver.Sum([self.x[i, j, k] for i in self.mitarbeiter]) >= self.min_anwesend[j][k])
+
+
+
+        # NB 3 - Max. Arbeitszeit pro Woche - (working_h muss noch berechnet werden!)
+        total_hours = {ma: self.solver.Sum([self.x[ma, j, k] for j in range(self.calc_time) for k in range(len(self.verfügbarkeit[ma][j]))]) for ma in self.mitarbeiter}
+        for ma in self.mitarbeiter:
+            self.solver.Add(self.total_hours[ma] <= self.working_h)
+
+
+        """ 
+        # NB X - Max. Arbeitszeit pro Tag - Diese NB ist nicht mehr nötig, da die max zeit bereits in der NB5 implementiert ist
+        for i in mitarbeiter:
+            for j in range(calc_time):
+                solver.Add(solver.Sum(x[i, j, k] for k in range(len(verfügbarkeit[i][j]))) <= max_zeit[i])
+        """
+
+
+        # NB 4 - Min. und Max. Arbeitszeit pro Tag
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):
+                # Prüfen, ob der Mitarbeiter an diesem Tag arbeiten kann (z.B. [0, 1, 1] = sum(2))
+                if sum(self.verfügbarkeit[i][j]) >= self.min_zeit[i]:
+                    sum_hour = self.solver.Sum(self.x[i, j, k] for k in range(len(self.verfügbarkeit[i][j])))
+                    # Es ist nötig, das die min und die max Zeit implementiert ist. 
+                    self.solver.Add(sum_hour >= self.min_zeit[i] * self.a[i, j])
+                    # NB 4.1 - Die Arbeitszeit eines Mitarbeiters an einem Tag kann nicht mehr als die maximale Arbeitszeit pro Tag betragen
+                    self.solver.Add(sum_hour <= self.max_zeit[i] * self.a[i, j])
+
+        
+
+        # NB 5 - Anzahl Arbeitsblöcke
+        for i in self.mitarbeiter:
+            for j in range(self.calc_time):
+                # Für die erste Stunde des Tages
+                self.solver.Add(self.y[i, j, 0] >= self.x[i, j, 0])
+                # Für die restlichen Stunden des Tages
+                for k in range(1, len(self.verfügbarkeit[i][j])):
+                    self.solver.Add(self.y[i, j, k] >= self.x[i, j, k] - self.x[i, j, k-1])
+                # Die Summe der y[i, j, k] für einen bestimmten Tag j sollte nicht größer als 1 sein
+                self.solver.Add(self.solver.Sum(self.y[i, j, k] for k in range(len(self.verfügbarkeit[i][j]))) <= 1)
+        
+
+        # NB X - Innerhalb einer Woche immer gleiche Schichten
+        
+        
+        # NB 6 - Verteilungsgrad MA - (entsprechend employment_lvl (keine Festanstellung) - muss noch angepasst werden, sobald feste MA eingeplant werden)
+        list_gesamtstunden = []
+        prozent_gesamtstunden = []
+        gerechte_verteilung = []
+        for i in range(len(self.mitarbeiter)):
+            if self.gesamtstunden_verfügbarkeit[i] > self.working_h:
+                arbeitsstunden_MA = self.employment_lvl[i] * self.working_h
+            else:
+                arbeitsstunden_MA = self.employment_lvl[i] * self.gesamtstunden_verfügbarkeit[i]
+            list_gesamtstunden.append(int(arbeitsstunden_MA))
+        # list_gesamtstunden = [35, 28, 28, 21, 21]
+        summe_stunden = sum(list_gesamtstunden)
+        # summe_stunden = 133
+        
+        for i in range(len(self.mitarbeiter)):
+            prozent_per_ma = list_gesamtstunden[i] / summe_stunden
+            prozent_gesamtstunden.append(prozent_per_ma)
+         # prozent_gesamtstunden = [0.2631578947368421, 0.21052631578947367, 0.21052631578947367, 0.15789473684210525, 0.15789473684210525]
+
+        for i in range(len(self.mitarbeiter)):
+            if self.employment[i] == "Perm":
+                verteilende_h = self.working_h
+            else:
+                verteilende_h = prozent_gesamtstunden[i] * self.verteilbare_stunden
+                # +0.5, damit es immer aufgerundet
+            gerechte_verteilung.append(round(verteilende_h + 0.5))
+        print("Gerechte Verteilung: ", gerechte_verteilung)     
+
+        # for loop für die gerechte Verteilung gemäss Liste gerechte_verteilung
+        verteilungsstunden = {ma: self.solver.Sum([self.x[ma, j, k] for j in range(self.calc_time) for k in range(len(self.verfügbarkeit[ma][j]))]) for ma in self.mitarbeiter}
+        
+        # Toleranz später noch auslagern
+        tolerance = 0.3
+        for i, ma in enumerate(self.mitarbeiter):
+            lower_bound = gerechte_verteilung[i] * (1 - tolerance)
+            upper_bound = gerechte_verteilung[i] * (1 + tolerance)
+            self.solver.Add(verteilungsstunden[ma] <= upper_bound)
+            self.solver.Add(verteilungsstunden[ma] >= lower_bound)
+        
+
+        # NB 7 - Feste Mitarbeiter zu employement_level fest einplanen
+        total_hours = {ma: self.solver.Sum([self.x[ma, j, k] for j in range(self.calc_time) for k in range(len(self.verfügbarkeit[ma][j]))]) for ma in self.mitarbeiter}
+        for i, ma in enumerate(self.mitarbeiter):
+            if self.employment[i] == "Perm": 
+                self.solver.Add(total_hours[ma] == self.working_h)
+
+
+        # NB X - Wechselnde Schichten innerhalb 2 Wochen
+
+        # NB X - Gleiche Verteilung der Stunden über eine Woche
+
+
+def solve_problem(self):
+    """
+    Problem lösen
+    """
+
+    self.solver.EnableOutput()
+    self.status = self.solver.Solve()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def algorithm(self):
         
