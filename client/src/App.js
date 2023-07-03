61826b8fa3905b25f0e0dfdcd6516ab5b2cff57b
masterybyme <134199096+masterybyme@users.noreply.github.com>
2023-07-03 19:11:32 +0000

    logout cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import { useState, useEffect, useContext } from "react";
-import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
-import { useLocation, Navigate } from "react-router-dom";
-import AuthProvider from "./AuthContext";
+import AuthProvider, { AuthContext } from "./AuthContext";
 import PrivateRoute from "./PrivateRoute";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
@@ -26,18 +24,12 @@ import Calendar from "./scenes/calendar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import axios from "axios";
-import { AuthContext } from "./AuthContext";
-
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
   const [users, setUsers] = useState([]);
   const [message, setMessage] = useState("");
-  const location = useLocation();
-  const isAuthenticated = location.pathname !== "/"; // Check if the user is authenticated
-  // const { user } = useContext(AuthContext);
-  // const isAuthenticated = !!user;
 
   return (
     <AuthProvider>
@@ -45,34 +37,45 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <div className="app">
-            {isAuthenticated && isSidebar && <Sidebar />}
-            <main className="content">
-              {isAuthenticated && <Topbar />}
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<PrivateRoute component={Dashboard} accessLevel="Admin" />} />
-                <Route path="/availability" element={<PrivateRoute component={Availability} accessLevel="Admin" />} />
-                <Route path="/Team" element={<PrivateRoute component={Team} accessLevel="Admin" />} />
-                <Route path="/Update" element={<PrivateRoute component={Update} accessLevel="Admin" />} />
-                <Route path="/Invite" element={<PrivateRoute component={Invite} accessLevel="Admin" />} />
-                <Route path="/Invoices" element={<PrivateRoute component={Invoices} accessLevel="Admin" />} />
-                <Route path="/company" element={<PrivateRoute component={Company} accessLevel="Admin" />} />
-                <Route path="/Contacts" element={<PrivateRoute component={Contacts} accessLevel="Admin" />} />
-                <Route path="/Bar" element={<PrivateRoute component={Bar} accessLevel="Admin" />} />
-                <Route path="/Form" element={<PrivateRoute component={Form} accessLevel="Admin" />} />
-                <Route path="/Line" element={<PrivateRoute component={Line} accessLevel="Admin" />} />
-                <Route path="/Pie" element={<PrivateRoute component={Pie} accessLevel="Admin" />} />
-                <Route path="/FAQ" element={<PrivateRoute component={FAQ} accessLevel="Admin" />} />
-                <Route path="/Plan" element={<PrivateRoute component={Plan} accessLevel="Admin" />} />
-                <Route path="/Geography" element={<PrivateRoute component={Geography} accessLevel="Admin" />} />
-                <Route path="/Calendar" element={<PrivateRoute component={Calendar} accessLevel="Admin" />} />
-              </Routes>
-            </main>
+            <AppContent isSidebar={isSidebar} setIsSidebar={setIsSidebar} />
           </div>
         </ThemeProvider>
       </ColorModeContext.Provider>
     </AuthProvider>
+  );
+}
+
+function AppContent({ isSidebar, setIsSidebar }) {
+  const { user } = useContext(AuthContext);
+  const isAuthenticated = !!user;
+
+  return (
+    <>
+      {isAuthenticated && isSidebar && <Sidebar />}
+      <main className="content">
+        {isAuthenticated && <Topbar />}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute component={Dashboard} accessLevel="Admin" />} />
+          <Route path="/availability" element={<PrivateRoute component={Availability} accessLevel="Admin" />} />
+          <Route path="/Team" element={<PrivateRoute component={Team} accessLevel="Admin" />} />
+          <Route path="/Update" element={<PrivateRoute component={Update} accessLevel="Admin" />} />
+          <Route path="/Invite" element={<PrivateRoute component={Invite} accessLevel="Admin" />} />
+          <Route path="/Invoices" element={<PrivateRoute component={Invoices} accessLevel="Admin" />} />
+          <Route path="/company" element={<PrivateRoute component={Company} accessLevel="Admin" />} />
+          <Route path="/Contacts" element={<PrivateRoute component={Contacts} accessLevel="Admin" />} />
+          <Route path="/Bar" element={<PrivateRoute component={Bar} accessLevel="Admin" />} />
+          <Route path="/Form" element={<PrivateRoute component={Form} accessLevel="Admin" />} />
+          <Route path="/Line" element={<PrivateRoute component={Line} accessLevel="Admin" />} />
+          <Route path="/Pie" element={<PrivateRoute component={Pie} accessLevel="Admin" />} />
+          <Route path="/FAQ" element={<PrivateRoute component={FAQ} accessLevel="Admin" />} />
+          <Route path="/Plan" element={<PrivateRoute component={Plan} accessLevel="Admin" />} />
+          <Route path="/Geography" element={<PrivateRoute component={Geography} accessLevel="Admin" />} />
+          <Route path="/Calendar" element={<PrivateRoute component={Calendar} accessLevel="Admin" />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
