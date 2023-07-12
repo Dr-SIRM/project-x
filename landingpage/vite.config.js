c2e4306c0d22c7d3300615718561154f53b9c82d
masterybyme <134199096+masterybyme@users.noreply.github.com>
2023-07-12 18:56:10 +0000

    landingpage

--- landingpage/vite.config.js
+++ landingpage/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // vite.config.js
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: '0.0.0.0'
   }
-}
-
+})
