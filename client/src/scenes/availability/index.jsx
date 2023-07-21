9d4d94632e6afb3670d4a2c88ae73decc7856ef5
masterybyme <134199096+masterybyme@users.noreply.github.com>
2023-07-21 07:44:21 +0000

    availability changes

--- client/src/scenes/availability/index.jsx
+++ client/src/scenes/availability/index.jsx
@@ -1,6 +1,253 @@
 import { useState, useEffect } from "react";
 import { useTheme, Box, Button, TextField, Snackbar, Typography } from "@mui/material";
 import { Formik } from "formik";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import axios from 'axios';
+
+const Availability = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [showSuccessNotification, setShowSuccessNotification] = useState(false);
+  const [showErrorNotification, setShowErrorNotification] = useState(false);
+  const [availabilityData, setAvailabilityData] = useState({});
+
+  useEffect(() => {
+    const fetchAvailabilityData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/availability');
+        setAvailabilityData(response.data);
+      } catch (error) {
+        console.error('Error fetching Availability details:', error);
+      }
+    };
+
+    fetchAvailabilityData();
+  }, []);
+
+  
+  const handleFormSubmit = async (values) => {
+    try {
+      // Send the updated form values to the server for database update
+      await axios.post('http://localhost:5000/api/availability', values);
+      setShowSuccessNotification(true);
+    } catch (error) {
+      console.error('Error updating availability details:', error);
+      setShowErrorNotification(true);
+    }
+  };
+
+
+  return (
+    <Box m="20px">
+      <Header
+        title="Availability"
+        subtitle="Please update your availability data whenever necessary. These are the basics for your optimized Scheduler."
+      />
+      <h2>Availability Sheet</h2>
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        enableReinitialize={true}
+        initialValues={{
+          ...Object.keys(availabilityData).reduce((acc, key) => {
+            acc[key] = availabilityData[key] || "";
+            return acc;
+          }, {}),
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(7, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 6" },
+              }}
+            >
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                Weekday
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                Start Time 1
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                End Time 1
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                Start Time 2
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                End Time 2
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                Start Time 3
+              </Typography>
+              <Typography
+                color={colors.greenAccent[500]}
+                variant="h6"
+                sx={{
+                  gridColumn: "span 1",
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                End Time 3
+              </Typography>
+              {Array.from({ length: 7 }).map((_, rowIndex) => (
+                <>
+                  <Typography
+                    key={`number-${rowIndex}`}
+                    color={colors.greenAccent[500]}
+                    variant=""
+                    sx={{
+                      gridColumn: "span 1",
+                      display: "flex",
+                      alignItems: "center",
+                      height: "100%",
+                    }}
+                  >
+                    Day {rowIndex + 1}
+                  </Typography>
+                  {Array.from({ length: 6 }).map((_, columnIndex) => (
+                    <TextField
+                      key={`day_${rowIndex + 1}_${columnIndex}`}
+                      fullWidth
+                      variant="filled"
+                      type="time"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values[`day_${rowIndex + 1}_${columnIndex}`] || ""}
+                      name={`day_${rowIndex + 1}_${columnIndex}`}
+                      error={
+                        !!touched[`day_${rowIndex + 1}_${columnIndex}`] &&
+                        !!errors[`day_${rowIndex + 1}_${columnIndex}`]
+                      }
+                      helperText={
+                        touched[`day_${rowIndex + 1}_${columnIndex}`] &&
+                        errors[`day_${rowIndex + 1}_${columnIndex}`]
+                      }
+                      sx={{ gridColumn: "span 1" }}
+                    />
+                  ))}
+                </>
+              ))}
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Update
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+      <Snackbar
+        open={showSuccessNotification}
+        onClose={() => setShowSuccessNotification(false)}
+        message="Availability updated successfully"
+        autoHideDuration={3000}
+        sx={{
+          backgroundColor: "green !important",
+          color: "white",
+          "& .MuiSnackbarContent-root": {
+            borderRadius: "4px",
+            padding: "15px",
+            fontSize: "16px",
+          },
+        }}
+      />
+      <Snackbar
+        open={showErrorNotification}
+        onClose={() => setShowErrorNotification(false)}
+        message="Error occurred - Please check your data and try again"
+        autoHideDuration={3000}
+        sx={{
+          backgroundColor: "red !important",
+          color: "white",
+          "& .MuiSnackbarContent-root": {
+            borderRadius: "4px",
+            padding: "15px",
+            fontSize: "16px",
+          },
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Availability;
+
+
+{/*
+import { useState, useEffect } from "react";
+import { useTheme, Box, Button, TextField, Snackbar, Typography } from "@mui/material";
+import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
@@ -254,3 +501,4 @@ const Availability = ({ availability }) => {
 
 
 export default Availability;
+  Comment */} 
