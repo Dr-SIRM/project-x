c36508741520d0a0ac444a52fc0a41ec3d9515c4
gerymueller2 <134267003+gerymueller2@users.noreply.github.com>
2023-07-29 12:30:29 +0000

    Änderungen

--- or_algorithm.py
+++ or_algorithm.py
@@ -151,7 +151,7 @@ class ORAlgorithm:
 
         # -- 3 --
         # Kosten für jeden MA noch gleich, ebenfalls die max Zeit bei allen gleich
-        self.kosten = {ma: 10 for ma in self.mitarbeiter}  # Kosten pro Stunde
+        self.kosten = {ma: 20 for ma in self.mitarbeiter}  # Kosten pro Stunde
 
         # -- 4 --
         self.max_zeit = {ma: 9*4 for ma in self.mitarbeiter}  # Maximale Arbeitszeit pro Tag
