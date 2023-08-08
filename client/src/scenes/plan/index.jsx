09f74bb2d2c73de012d8f23887920664c25311ca
masterybyme <134199096+masterybyme@users.noreply.github.com>
2023-08-08 16:33:37 +0000

    schichtplan

--- client/src/scenes/plan/index.jsx
+++ client/src/scenes/plan/index.jsx
@@ -17,7 +17,7 @@ const MyTimeline = () => {
 
       const items = new DataSet([
         { id: 1, group: 1, content: 'test', start: '2022-08-08', end: '2022-08-08 12:00:00' },
-        { id: 2, group: 2, content: '', start: '2022-08-09', end: '2022-08-09 14:00:00' },
+        { id: 2, group: 2, content: 'big fat titties', start: '2022-08-09', end: '2022-08-09 14:00:00' },
         // Weitere Items hinzufügen
       ]);
 
