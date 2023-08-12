1b67feecfc40836374efde94977d26246817bb19
Robin <robin_england@hotmail.com>
2023-08-12 13:22:30 +0200

    schichtplan

--- client/src/scenes/plan/index.jsx
+++ client/src/scenes/plan/index.jsx
@@ -1,51 +1,111 @@
-import React, { useEffect, useRef } from 'react';
-import { Box, Typography, useTheme } from "@mui/material";
-import '../../App.css';
-import { DataSet } from 'vis-data';
-import { Timeline } from 'vis-timeline';
-import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Box } from "@mui/material";
+import 'dhtmlx-gantt/codebase/dhtmlxgantt.css'; // Import Gantt styles
 import Header from "../../components/Header";
+import gantt from 'dhtmlx-gantt'; // Import the Gantt library
+import { createEvents } from 'ics'; // Import the createEvents function from ics library
+import jsPDF from 'jspdf';
 
-const MyTimeline = () => {
+const MyGanttChart = () => {
   const containerRef = useRef(null);
+  const [currentView, setCurrentView] = useState('day'); // 'day', 'week', 'month'
 
   useEffect(() => {
     if (containerRef.current) {
-      const groups = new DataSet([
-        { id: 1, content: 'Max Mustermann' },
-        { id: 2, content: 'Erika Mustermann' },
-        // Weitere Gruppen/Mitarbeiter hinzufügen
-      ]);
+      // Initialize Gantt chart
+      gantt.config.xml_date = '%Y-%m-%d %H:%i:%s';
+      gantt.config.readonly = true; // Disable editing interactions
+      gantt.config.buttons_left = []; // Remove "Add Task" button
+      gantt.config.lightbox.sections = []; // Remove lightbox sections (for task editing)
 
-      const items = new DataSet([
-        { id: 1, group: 1, content: 'test', start: '2022-08-08', end: '2022-08-08 12:00:00' },
-        { id: 2, group: 2, content: 'big fat titties', start: '2022-08-09', end: '2022-08-09 14:00:00' },
-        // Weitere Items hinzufügen
-      ]);
-
-      const options = {
-        orientation: 'top',
-        verticalScroll: true,
-        zoomable: true,
+      // Set up timeline configuration based on the current view
+      const timelineConfig = {
+        day: { scale_unit: 'hour', step: 1, date: '%H:%i' }, // Show hours
+        week: { scale_unit: 'day', step: 1, date: '%d %M' }, // Show days
+        month: { scale_unit: 'month', step: 1, date: '%F' }
       };
 
-      const timeline = new Timeline(containerRef.current, items, groups, options);
+      gantt.config.scale_unit = timelineConfig[currentView].scale_unit;
+      gantt.config.step = timelineConfig[currentView].step;
+      gantt.config.date_scale = timelineConfig[currentView].date;
 
-  // Bereinigungsfunktion
+      gantt.init(containerRef.current);
+
+      // Define tasks and links
+      const tasks = [
+        { id: 1, text: 'Shift 1', start_date: '2022-08-08 08:00', end_date: '2022-08-08 12:00' }, // Example: Show in hours
+        { id: 2, text: 'Shift 2', start_date: '2022-08-09 10:00', end_date: '2022-08-09 14:00' }, // Example: Show in hours
+        // Add more shifts
+      ];
+
+      const links = [
+        // Add your links here
+      ];
+
+      gantt.parse({ data: tasks, links });
+
+      // Clean up function
       return () => {
-        timeline.destroy();
+        gantt.clearAll();
       };
     }
-  }, []);
+  }, [currentView]);
+
+  const switchToDayView = () => {
+    setCurrentView('day');
+  };
+
+  const switchToWeekView = () => {
+    setCurrentView('week');
+  };
+
+  const switchToMonthView = () => {
+    setCurrentView('month');
+  };
+
+  const exportAsPDF = () => {
+    const ganttContainer = containerRef.current;
+    const pdf = new jsPDF('p', 'pt', 'a4');
+
+    pdf.html(ganttContainer, {
+      callback: () => {
+        pdf.save('gantt_chart.pdf');
+      },
+    });
+  };
+
+  const exportToOutlook = () => {
+    const events = [
+      { start: [2022, 8, 8, 8, 0], end: [2022, 8, 8, 12, 0], title: 'Shift 1' },
+      { start: [2022, 8, 9, 10, 0], end: [2022, 8, 9, 14, 0], title: 'Shift 2' },
+      // Add more events
+    ];
+
+    const { error, value } = createEvents(events);
+    if (!error) {
+      const blob = new Blob([value], { type: 'text/calendar;charset=utf-8' });
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'shifts.ics';
+      link.click();
+    }
+  };
 
   return (
-  <Box m="20px">
-    <Header title="Schichtplan" subtitle="Übersicht von den eingeteilten Schichten / Mitglieder" /> 
-    <Box 
-      ref={containerRef} style={{ height: '400px' }}>
+    <Box m="20px">
+      <Header title="Shift Plan" subtitle="Overview of scheduled shifts for members" />
+      <div>
+        <button onClick={switchToDayView}>Day View</button>
+        <button onClick={switchToWeekView}>Week View</button>
+        <button onClick={switchToMonthView}>Month View</button>
+        <button onClick={exportAsPDF}>Export as PDF</button>
+        <button onClick={exportToOutlook}>Export to Outlook</button>
+      </div>
+      <div style={{ position: 'relative' }}>
+        <Box ref={containerRef} style={{ height: '400px' }} />
+      </div>
     </Box>
-  </Box>
   );
 };
 
-export default MyTimeline;
+export default MyGanttChart;
