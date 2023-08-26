79d8793f18a5bfc078a88438d3dbfcef0fc3b270
Phu <nguyen.phu@gmx.de>
2023-08-26 12:52:54 +0200

    update

--- client/src/scenes/company/index.jsx
+++ client/src/scenes/company/index.jsx
@@ -204,7 +204,7 @@ const Company = ({ company }) => {
                 >
                 <MenuItem value={ '1' }>1</MenuItem>
                 <MenuItem value={ '2' }>2</MenuItem>
-                <MenuItem value={ '4' }>4</MenuItem>
+                <MenuItem value={ '3' }>3</MenuItem>
                 </Select>
               <Typography
                 color={colors.greenAccent[500]}
