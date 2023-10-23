f763bd17a8b22a05c8e978ec4f2d10beda6994ac
Gery <gerymueller@hotmail.com>
2023-10-23 20:06:32 +0200

    Vorüberprüfungen auskommentiert, zu testzwecken

--- routes_react.py
+++ routes_react.py
@@ -794,6 +794,7 @@ def run_solver():
 
         or_algo_cp.run()
     
+        """
         errors = []  # Eine Liste um alle Fehler zu speichern
         
         for i in range(1, 7):  # Assuming you have 6 pre-checks
@@ -811,6 +812,7 @@ def run_solver():
         # Wenn Fehler während der Überprüfungen aufgetreten sind, werden diese hier gesendet.
         if errors:
             return jsonify({'message': errors}), 400
+        """
 
         # Wenn keine Fehler aufgetreten sind, wird der Algorithmus weiter durchgeführt.
         or_algo_cp.run_2()
